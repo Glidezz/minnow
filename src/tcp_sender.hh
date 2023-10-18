204d4fb1327a6c3b4aeec5a6e53d4411325b5196
xiao-chuachua <U202115586@hust.edu.cn>
2023-10-18 14:36:00 +0800

    lab3

--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,10 +4,61 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+#include <queue>
+
+class Timer
+{
+private:
+  uint64_t initial_RTO_ms;
+  uint64_t current_RTO_ms;
+  uint64_t time_ms { 0 };
+  bool running { false };
+
+public:
+  explicit Timer( uint64_t init_ROT ) : initial_RTO_ms( init_ROT ), current_RTO_ms( init_ROT ) {}
+
+  void stop() { running = false; }
+
+  void start()
+  {
+    time_ms = 0;
+    running = true;
+  }
+
+  void tick( uint64_t ms_since_last_tick )
+  {
+    if ( running ) {
+      time_ms += ms_since_last_tick;
+    }
+  }
+
+  bool is_running() { return running; }
+
+  bool is_expired() { return running && time_ms >= current_RTO_ms; }
+
+  void doubleRTO() { current_RTO_ms *= 2; }
+
+  void resetRTO() { current_RTO_ms = initial_RTO_ms; }
+};
+
 class TCPSender
 {
   Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+  uint64_t initial_RTO_ms_; //超时时长
+
+  bool syn_ { false };
+  bool fin_ { false };
+  uint64_t retransmit_cnt_ { 0 }; // 超时重复发送的数量
+
+  uint64_t acked_seqno { 0 };
+  uint64_t next_seqno { 0 };
+  uint64_t window_size { 1 };
+  uint64_t outstanding_cnt { 0 }; // 发送中的数量
+
+  std::queue<TCPSenderMessage> outstanding_segments {}; //发送中的队列 发送中未被确认的队列
+  std::queue<TCPSenderMessage> queued_segments {};      // 缓存队列 标识还未被发送的队列
+
+  Timer timer { initial_RTO_ms_ };
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
