c4ea30c8e9e934f2b2b37efc39c995e260a1f8f4
xiao-chuachua <U202115586@hust.edu.cn>
2023-11-12 14:21:29 +0800

    lab4

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -18,15 +18,18 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
       zero_point        :: Initial Sequence Number
       checkpoint        :: "the first unassembled index"
   */
-  uint64_t offset = this->raw_value_ - zero_point.raw_value_;
-  uint64_t temp = ( checkpoint & 0xffffffff00000000 ) + offset;
-  uint64_t ret = temp;
-  if ( abs( int64_t( temp + ( 1ul << 32 ) - checkpoint ) ) < abs( int64_t( temp - checkpoint ) ) ) {
-    ret = ret + ( 1ul << 32 );
-  }
-  if ( temp >= ( 1ul << 32 )
-       && abs( int64_t( temp - ( 1ul << 32 ) - checkpoint ) ) < abs( int64_t( ret - checkpoint ) ) ) {
-    ret = temp - ( 1ul << 32 );
-  }
-  return ret;
+
+  /*这段是受到清华大佬(huangrt01)博客的启发*/
+  uint32_t offset = this->raw_value_ - wrap( checkpoint, zero_point ).raw_value_;
+  uint64_t result = checkpoint + offset;
+  /*
+    如果新位置距离checkpoint的偏移offset大于1<<32的一半也就是1<<31,
+    那么离checkpoint最近的应该是checkpoint前面的元素
+    举个例子: 1---------7(checkpoint)----------------1<<32+1;
+    由于是无符号数相减所以1-7 == 1<<32+1 - 7;
+    所以应该是1距离7最近所以应该选1
+  */
+  if ( offset > ( 1u << 31 ) && result >= ( 1ul << 32 ) )
+    result -= ( 1ul << 32 );
+  return result;
 }
